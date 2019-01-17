6b982f4ec1c180e4f97325d6234b1cf948730812
liulinfan <361087115@qq.com>
2019-01-17 10:26:13 +0800

    first init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+   <p>刘林凡先生&赵婉君女士结婚照</p>
+    <img :src="myImg">
+  </div>
+</template>
+
+<script>
+import imgUrl from '@/assets/pic.jpeg'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      myImg: imgUrl
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
